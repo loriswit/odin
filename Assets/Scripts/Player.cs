6b28edda3a12bb1d28365a2006bb6e7009b945bc
Loris Witschard <loris.wit@gmail.com>
2021-04-28 23:13:11 +0200

    fix(player): improve walking on slopes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,45 +15,80 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float maxGroundAngle = 45;
 
-    [Header("Collider Material")]
-    [SerializeField]
-    private PhysicsMaterial2D groundedMaterial;
-
-    [SerializeField]
-    private PhysicsMaterial2D midairMaterial;
-
     private Rigidbody2D body;
-    private new CapsuleCollider2D collider;
 
+    private float gravityScale;
     private Vector2 velocity;
     private float direction;
+    private float jumpCooldown;
 
     private GameObject ground;
-    private bool Grounded => ground != null;
+    private Vector2 groundNormal;
 
     private const float MoveSmoothing = 0.05f;
+    private const float JumpDelay = 0.1f;
+
+    /**
+     * True whenever the player is on the ground.
+     */
+    private bool Grounded => ground != null;
+
+    /**
+     * True whenever the player is in the air.
+     * Also true during cooldown to prevents ground-related behaviours right after jumping.
+     */
+    private bool MidAir => !Grounded || jumpCooldown > 0;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-        collider = GetComponent<CapsuleCollider2D>();
+
+        // disable default gravity
+        gravityScale = body.gravityScale;
+        body.gravityScale = 0;
     }
 
+    /**
+     * Physics-related updates.
+     */
     private void FixedUpdate()
     {
-        // set speed according to current direction
-        var current = body.velocity;
-        var target = new Vector2(direction * moveSpeed, current.y);
-        body.velocity = Vector2.SmoothDamp(current, target, ref velocity, MoveSmoothing);
+        // decrease cooldown
+        jumpCooldown -= Time.fixedDeltaTime;
 
-        // set appropriate physics material
-        collider.sharedMaterial = Grounded ? groundedMaterial : midairMaterial;
+        Vector2 targetVelocity;
+        Vector2 gravity;
+
+        // when mid-air, move horizontally and use default gravity
+        if (MidAir)
+        {
+            targetVelocity = new Vector2(direction * moveSpeed, body.velocity.y);
+            gravity = Physics.gravity;
+        }
+
+        // when on ground, define velocity and gravity according to slope angle
+        // this makes walking on slopes more smooth, as if the ground was always horizontal
+        else
+        {
+            targetVelocity = Vector2.Perpendicular(groundNormal) * (direction * moveSpeed);
+            gravity = groundNormal * Physics.gravity.magnitude;
+        }
+
+        body.AddForce(gravity * (gravityScale * body.mass));
+        body.velocity = Vector2.SmoothDamp(body.velocity, targetVelocity, ref velocity, MoveSmoothing);
+
+        // draw gravity and velocity vectors
+        Debug.DrawRay(body.position, gravity, Color.white);
+        Debug.DrawRay(body.position, targetVelocity, Color.grey);
+        Debug.DrawRay(body.position, body.velocity, Color.black);
     }
 
     private void OnJump()
     {
-        if (Grounded)
-            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        if (MidAir) return;
+
+        body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        jumpCooldown = JumpDelay;
     }
 
     private void OnMove(InputValue value)
@@ -63,16 +97,24 @@ public class Player : MonoBehaviour
         direction = value.Get<float>();
     }
 
-
     private void OnCollisionStay2D(Collision2D collision)
     {
-        // remove reference if not ground anymore
-        if (Grounded && collision.gameObject == ground && !IsGround(collision))
+        // check if colliding with ground
+        if (!Grounded || Grounded && collision.gameObject == ground)
+        {
             ground = null;
+            groundNormal = Vector2.zero;
 
-        // check for new ground
-        else if (!Grounded && IsGround(collision))
-            ground = collision.gameObject;
+            foreach (var contact in collision.contacts)
+            {
+                var angle = Vector2.Angle(Vector2.up, contact.normal);
+                if (angle > maxGroundAngle) continue;
+
+                ground = collision.gameObject;
+                groundNormal = -contact.normal;
+                break;
+            }
+        }
 
         // draw contact points
         foreach (var contact in collision.contacts)
@@ -85,12 +127,5 @@ public class Player : MonoBehaviour
         // leave ground
         if (Grounded && collision.gameObject == ground)
             ground = null;
-    }
-
-    private bool IsGround(Collision2D collision)
-    {
-        return collision.contacts
-            .Select(contact => Vector2.Angle(Vector2.up, contact.normal))
-            .Any(angle => angle < maxGroundAngle);
     }
 }
