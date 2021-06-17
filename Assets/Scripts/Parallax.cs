b71472790654ab9df20f6c76e98d0e1dd4029de1
Loris Witschard <loris.wit@gmail.com>
2021-06-17 18:11:54 +0200

    feat(level): add graphics to start zone

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]
+    private float depth = 10;
+
+    private Transform cameraTransform;
+    private Vector3 origin;
+    private Vector3 cameraOrigin;
+
+    private void Awake()
+    {
+        origin = transform.position;
+        cameraTransform = Camera.main.transform;
+        cameraOrigin = cameraTransform.position;
+    }
+
+    private void Update()
+    {
+        var target = origin + (cameraTransform.position - origin) * (1 / depth);
+        target.z = origin.z;
+        transform.position = target;
+    }
+}
